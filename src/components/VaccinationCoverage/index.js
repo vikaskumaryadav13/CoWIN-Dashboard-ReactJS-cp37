a7a38ba03b4372fb107218a4a86b67c3c7e3abc1
vikasku664@gmail.com <vikasku664@gmail.com>
2024-02-13 06:20:12 +0000

    CoWIN Dashboard

---
+++ src/components/VaccinationCoverage/index.js
@@ -0,0 +1,71 @@
+// Write your code here
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
+
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const dataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+  const {vaccinationCoverageDetails} = props
+
+  return (
+    <div className="chart-bg-container">
+      <h1 className="chart-heading">Vaccination Coverage</h1>
+      <BarChart
+        width={900}
+        height={400}
+        data={vaccinationCoverageDetails}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 1,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <YAxis
+          tickFormatter={dataFormatter}
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 0.5,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            paddingTop: 20,
+            fontSize: 12,
+            fontFamily: 'Roboto',
+            textAlign: 'center',
+          }}
+        />
+        <Bar
+          dataKey="dose1"
+          name="Dose 1"
+          fill="#5a8dee"
+          barSize="20%"
+          radius={[10, 10, 0, 0]}
+        />
+        <Bar
+          dataKey="dose2"
+          name="Dose 2"
+          fill="#f54394"
+          barSize="20%"
+          radius={[5, 5, 0, 0]}
+        />
+      </BarChart>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
